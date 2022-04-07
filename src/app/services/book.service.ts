6a57fbda56a00d52e777fbf65fc64039b32788b9
kensinaon <kensinaon@gmail.com>
2022-04-07 12:42:49 +0800

    initial

---
+++ src/app/services/book.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, Observer } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Book } from '../models/book.model';
+import { AuthenticationService } from './authentication.service';
+import { RequestBaseService } from './request-base.service';
+
+const API_URL = `${environment.BASE_URL}/api/book`;
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class BookService extends RequestBaseService {
+
+  constructor(authenthicationService: AuthenticationService, http: HttpClient) {
+    super(authenthicationService, http);
+  }
+
+  saveBook(book: Book): Observable<any>{
+    return this.http.post(API_URL, book, {headers: this.getHeaders});
+  }
+
+  deleteBook(book:Book): Observable<any>{
+    return this.http.delete(`${API_URL}/${book.id}`, {headers: this.getHeaders});
+  }
+
+  getAllBooks(): Observable<any>{
+    return this.http.get(API_URL);
+  }
+}
